bcbd917ce5bad0afd0f0c7f44afa6886ef8a412c
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-28 14:52:55 -0500

    trying to fix users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-  # def index
-  #   @user = User.all
-  #   render :index
-  # end
   
-  # def show
-  #   @user = User.find_by(id: params[:id])
-  #   render template: "users/show"
-  # end  
+  def index
+    @user = User.all
+    render :index
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    render template: "users/show"
+  end
 
   def new
     @user = User.new
@@ -18,31 +19,19 @@ class UsersController < ApplicationController
     @user = User.new(
       name: params[:user][:name],
       email: params[:user][:email],
-      password: params[:user][:password]
-      
+      password: params[:user][:password],
+      password_confirmation: params[:user][:password_confirmation]
     )
     if @user.save
-     #session[:user_id] = @user.id
-      redirect_to "/users"
+      session[:user_id] = @user.id
+      redirect_to "/pets"
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+
   
-  # def update
-  #   @user = User.find_by(id: params[:id])
-  #   @user.name = params[:name] || @user.name
-  #   @user.email = params[:email] || @user.email
-  #   @user.password = params[:password_digest] || @user.password
-  
-  #   @user.save!
-  #   render :show
-  # end
-  
-  # def destroy
-  #   @user = User.find_by(id: params[:id])
-  #   @user.destroy
-  #   render json: {message: "User has been successfully removed"}
-  # end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   get "/signup" => "users#new"	
   post "/users" => "users#create"
+  get "/users" => "users#index"
 
 
   resources :tasks
