750978a7cae5282cc4222c9e212e1e78a6ca3ca5
Kimber Ogden <mx.kimber@gmail.com>
2023-05-01 12:42:18 -0500

    updates

--- app/controllers/category_tasks_controller.rb
+++ app/controllers/category_tasks_controller.rb
@@ -3,10 +3,15 @@ class CategoryTasksController < ApplicationController
 
   def create
     @category = Category.find(params[:category_id])
-    @task = Task.find(params[:task_id])
-    @category.tasks << @task
-    redirect_to task_path(@task)
+    @category_task = @category.category_tasks.new(task_id: params[:task_id])
+
+    if @category_task.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
 
   def destroy
     @category = Category.find(params[:category_id])
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  belongs_to :user
+
   has_many :category_tasks
   has_many :tasks, through: :category_tasks
 
