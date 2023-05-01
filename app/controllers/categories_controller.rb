9e2d919a103c907fd778bc36ef24e87816a0339f
Kimber Ogden <mx.kimber@gmail.com>
2023-05-01 15:19:25 -0500

    customized

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,11 +16,11 @@ class CategoriesController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
+
     @category = Category.new(category_params)
-=======
+
     @category = Category.new(name: params[:category][:name])
->>>>>>> aa85b1aef0da886ff92531d64b3ca97bcc478830
+
     if @category.save
       # @task = Task.find(params[:task_id])
       # @category_task = CategoryTask.new(category_id: @category.id, task_id: @task.id)
@@ -33,11 +33,6 @@ class CategoriesController < ApplicationController
     #   render :new, status: :unprocessable_entity
     # end
   end
-<<<<<<< HEAD
-=======
-
->>>>>>> aa85b1aef0da886ff92531d64b3ca97bcc478830
-
 
   def destroy
     @category = Category.find_by(id: params[:id])
